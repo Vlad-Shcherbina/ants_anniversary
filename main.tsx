b7a95e1ffda6503c680c8fda772aa0bdc3f642ce
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2024-08-02 16:22:32 +0200

    Rudimentary ant file uploading

--- main.tsx
+++ main.tsx
@@ -1,11 +1,39 @@
 import "./vendor/preact/debug.js"; // should be first
 
-import { bang } from "./assert.js";
+import { assert, bang } from "./assert.js";
 import * as preact from "./vendor/preact/preact.js";
+import * as storage from "./storage.js";
 
-function App() {
-    return <div>Hello, world!</div>;
+function App(props: { db: IDBDatabase }) {
+    let { db } = props;
+    return <div>
+        <input type="file" id="fileInput" accept=".ant" onChange={(e) => {
+            let target = bang(e.target as HTMLInputElement);
+            let file = target.files?.[0];
+            if (!file) return;
+            console.log(file.name, file.type);
+
+            const reader = new FileReader();
+            reader.onload = async function(e) {
+                const contents = bang(e.target).result;
+                assert(typeof contents === "string", typeof contents);
+                let name = file.name;
+                if (file.name.endsWith(".ant")) {
+                    name = name.slice(0, -4);
+                }
+                let id = await storage.compute_ant_id(contents);
+                let ant = { id, name, source: contents };
+                let added = await storage.add_ant(db, ant);
+                console.log("added", added);  // TODO: show toast (added or duplicate)
+            };
+            reader.readAsText(file);
+        }}/>
+    </div>;
 }
 
-let root = bang(document.getElementById("root"));
-preact.render(<App/>, root);
+async function main() {
+    let db = await storage.init();
+    let root = bang(document.getElementById("root"));
+    preact.render(<App db={db}/>, root);
+}
+main();
---
+++ storage.ts
@@ -0,0 +1,58 @@
+import { assert } from "./assert.js";
+
+type Ant = {
+    id: string,
+    name: string,
+    source: string,
+}
+
+export async function init() {
+    let req = window.indexedDB.open("ants_anniversary", 1);
+    req.onupgradeneeded = (e) => {
+        console.log(`upgrade: ${e.oldVersion} -> ${e.newVersion}`);
+        assert(e.oldVersion === 0);
+        let db = req.result;
+        db.createObjectStore("ants", { keyPath: "id" });
+    };
+    let db = await idb_async(req);
+    db.onversionchange = () => {
+        assert(false, "live version change");
+    };
+    return db;
+}
+
+export async function add_ant(db: IDBDatabase, ant: Ant) {
+    let tx = db.transaction("ants", "readwrite");
+    try {
+        let store = tx.objectStore("ants");
+        let existing = await idb_async(store.get(ant.id));
+        if (existing !== undefined) {
+            return false;
+        }
+        await idb_async(store.add(ant));
+        return true;
+    } catch (e) {
+        tx.abort();
+        throw e;
+    }
+}
+
+export async function get_all_ants(db: IDBDatabase): Promise<Ant[]> {
+    let tx = db.transaction("ants", "readonly");
+    return await idb_async(tx.objectStore("ants").getAll());
+}
+
+export async function compute_ant_id(source: string) {
+    let msgBuffer = new TextEncoder().encode(source);
+    let hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    let hashArray = Array.from(new Uint8Array(hashBuffer));
+    let hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+}
+
+function idb_async<T>(request: IDBRequest<T>): Promise<T> {
+    return new Promise((resolve, reject) => {
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    });
+}
